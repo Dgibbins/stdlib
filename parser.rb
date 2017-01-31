aa041cb9c5c780a7fff430d05d91442aab86b985
Alex Flint <me@alexf.ca>
2017-01-31 14:38:39 -0500

    added sumamry and pairs

--- parser.rb
+++ parser.rb
@@ -1,8 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://ruby-doc.org/stdlib-2.4.0/toc.html"))
-list = doc.css("a.mature")
+stdlib_url = "http://ruby-doc.org/stdlib-2.4.0/"
+stdlib_doc = Nokogiri::HTML(open("http://ruby-doc.org/stdlib-2.4.0/toc.html"))
+list = stdlib_doc.css("a.mature")
 items = []
 names = []
 
@@ -14,4 +15,21 @@ list.each do |item|
   names += [item['href'].split("/").fetch(1)]
 end
 
-p pairs = names.zip(items)
+pairs = names.zip(items)
+
+pairs.each do |pair|
+
+  @url = stdlib_url + 'libdoc/' + pairs[0][0].to_s + '/rdoc/index.html'
+  @doc = Nokogiri::HTML(open(@url))
+  @link = stdlib_url + 'libdoc/' + pairs[0][0].to_s + '/rdoc/' + @doc.css("div#class-index a").first['href']
+  @doc_page = Nokogiri::HTML(open(@link))
+  @summary = @doc_page.css('#description p').first.text
+
+end
+
+
+# doc_url = stdlib_url + 'libdoc/' + pairs[0][0].to_s + '/rdoc/index.html'
+#
+# abbrev_doc = Nokogiri::HTML(open(doc_url))
+#
+# abbrev_doc.css("div#class-index a").first['href']
