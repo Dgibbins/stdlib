20b3e7e3c411ea1078e31fd7ce904880e1370aa4
Alex Flint <me@alexf.ca>
2017-01-31 17:06:43 -0500

    all kinds of fucked up shit

--- parser.rb
+++ parser.rb
@@ -1,35 +1,44 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-stdlib_url = "http://ruby-doc.org/stdlib-2.4.0/"
-stdlib_doc = Nokogiri::HTML(open("http://ruby-doc.org/stdlib-2.4.0/toc.html"))
-list = stdlib_doc.css("a.mature")
+stdlib_url = 'http://ruby-doc.org/stdlib-2.4.0/'
+stdlib_doc = Nokogiri::HTML(open('http://ruby-doc.org/stdlib-2.4.0/toc.html'))
+list = stdlib_doc.css('a.mature')
 items = []
 names = []
 
 list.each do |item|
-  items += [item['href']]
+    items += [item['href']]
 end
 
 list.each do |item|
-  names += [item['href'].split("/").fetch(1)]
+    names += [item['href'].split('/').fetch(1)]
 end
 
 pairs = names.zip(items)
+# new_pairs = pairs[28..101]
+summaries = []
 
-pairs.each do |pair|
+i = 0
+while i <= pairs.length
+    pairs.each do |pair|
+        pairs[0][0] = 'io/nonblock/' if pairs[0][0] == "io"
+        doc = Nokogiri::HTML(open(url))
+        link = stdlib_url + 'libdoc/' + pairs[0][0].to_s + 'rdoc/' + doc.css('div#class-index a').first['href']
+        doc_page = Nokogiri::HTML(open(link))
 
-  @url = stdlib_url + 'libdoc/' + pairs[0][0].to_s + '/rdoc/index.html'
-  @doc = Nokogiri::HTML(open(@url))
-  @link = stdlib_url + 'libdoc/' + pairs[0][0].to_s + '/rdoc/' + @doc.css("div#class-index a").first['href']
-  @doc_page = Nokogiri::HTML(open(@link))
-  @summary = @doc_page.css('#description p').first.text
-
+        puts i
+        if doc_page.css('#description p').empty?
+          summaries += ['No description provided']
+        else
+          summaries += [doc_page.css('#description p').first.text]
+        end
+        i += 1
+    end
 end
 
 
-# doc_url = stdlib_url + 'libdoc/' + pairs[0][0].to_s + '/rdoc/index.html'
-#
-# abbrev_doc = Nokogiri::HTML(open(doc_url))
-#
-# abbrev_doc.css("div#class-index a").first['href']
+
+http://ruby-doc.org/stdlib-2.4.0/libdoc/getoptlong/rdoc/index.html
+http://ruby-doc.org/stdlib-2.4.0/libdoc/io/nonblock/rdoc/index.html
